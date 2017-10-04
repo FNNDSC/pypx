fad8131126d879ff8e617391b34f60b4de8b97f2
nicolasrannou <npm.rannou@gmail.com>
2017-10-04 14:13:30 +0200

    0.10 - fix

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def readme():
         return f.read()
 
 setup(name='pypx',
-      version='0.9',
+      version='0.10',
       description='Wrapper around DCMTK for PACS related actions (echo, find, move and listen)',
       long_description=readme(),
       url='http://github.com/fnndsc/pypx',
