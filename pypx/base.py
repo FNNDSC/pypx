ab92fdc65dff240c60a5c76d58eb45c672a13f5c
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2019-09-24 17:46:19 -0400

    Update Base with more shared methods between find/move.

--- pypx/base.py
+++ pypx/base.py
@@ -1,44 +1,83 @@
+import  subprocess, re, collections
+import  pfmisc
+from    pfmisc._colors      import  Colors
+
 class Base():
-    """docstring for Echo."""
+    """
+    A Base class for pypx.
+    
+    This class is somewhat abstract/virtual in as much as it
+    provides a substrate for derived classes and in and of 
+    itself does very little.
+
+    """
+
     def __init__(self,arg):
+        """
+        Initialize some core self variables common to all derived classes.
+        """
+
         self.arg = arg
 
-        if 'aet' in arg:
-            self.aet = arg['aet']
-        else:
-            self.aet = 'CHRIS-ULTRON-AET'
+        if 'aet' in arg:        self.aet = arg['aet']
+        else:                   self.aet = 'CHRIS-ULTRON-AET'
 
-        if 'aec' in arg:
-            self.aec = arg['aec']
-        else:
-            self.aec = 'CHRIS-ULTRON-AEC'
+        if 'aec' in arg:        self.aec = arg['aec']
+        else:                   self.aec = 'CHRIS-ULTRON-AEC'
 
-        if 'server_ip' in arg:
-            self.server_ip = arg['server_ip']
-        else:
-            self.server_ip = '192.168.1.110'
+        if 'serverIP' in arg:   self.serverIP = arg['serverIP']
+        else:                   self.serverIP = '192.168.1.110'
 
-        if 'server_port' in arg:
-            self.server_port = arg['server_port']
-        else:
-            self.server_port = '4242'
-
-        if 'executable' in arg:
-            self.executable = arg['executable']
-        else:
-            self.executable = '/usr/bin/echoscu'
+        if 'serverPort' in arg: self.serverPort = arg['serverPort']
+        else:                   self.serverPort = '4242'
 
         self.response = {
             'status': 'error',
             'data': {}
         }
 
+        self.dp = pfmisc.debug(
+                    verbosity   = self.arg['verbosity'],
+                    within      = 'Base',
+                    syslog      = False
+        )
+
+    def systemlevel_run(self, opt, d_params):
+        """
+        Run the system command, based on the passed paramter dictionary
+        """
+        b_commandGen    = False
+        str_cmd         = ''
+        d_ret           = {}
+
+        # A function to execute to generate commands
+        f_commandGen    = None
+        for k,v in d_params.items():
+            if k == 'f_commandGen':
+                f_commandGen    = v
+                b_commandGen    = True 
+            else:
+                opt[k]  = v
+
+        if b_commandGen:
+            str_cmd         = f_commandGen(opt)
+            self.dp.qprint("\n%s" % str_cmd, level = 5, type = 'status')
+            raw_response    = subprocess.run(
+                                str_cmd, 
+                                stdout  = subprocess.PIPE, 
+                                stderr  = subprocess.STDOUT, 
+                                shell   = True
+            )
+            d_ret   = self.formatResponse(raw_response)
+
+        return d_ret
+
     def commandSuffix(self):
         # required parameters
-        command_suffix = ' -aec ' + self.aec
-        command_suffix += ' -aet ' + self.aet
-        command_suffix += ' ' + self.server_ip
-        command_suffix += ' ' + self.server_port
+        command_suffix =    ' -aec '    + self.aec
+        command_suffix +=   ' -aet '    + self.aet
+        command_suffix +=   ' '         + self.serverIP
+        command_suffix +=   ' '         + self.serverPort
 
         return command_suffix
 
@@ -52,5 +91,77 @@ class Base():
         if std != '':
             response['status'] = 'error'
             response['data'] = std
+
+        return response
+
+    def checkResponse(self, response):
+        std_split = response.split('\n')
+        info_count = 0
+        error_count = 0
+        for line in std_split:
+            if line.startswith('I: '):
+                info_count += 1
+            elif line.startswith('E: '):
+                error_count += 1
+
+        status = 'error'
+        if error_count == 0:
+            status = 'success'
+
+        return status
+
+    def parseResponse(self, response):
+        data = []
+
+        uid = 0
+        std_split = response.split('\n')
+
+        for line in std_split:
+            if line.startswith('I: ---------------------------'):
+                data.append({})
+                data[-1]['uid'] = {}
+                data[-1]['uid']['tag'] = 0
+                data[-1]['uid']['value'] = uid
+                data[-1]['uid']['label'] = 'uid'
+                uid += 1
+
+            elif line.startswith('I: '):
+                lineSplit = line.split()
+                if len(lineSplit) >= 8 and re.search('\((.*?)\)', lineSplit[1]) != None:
+                    # extract DICOM tag
+                    tag = re.search('\((.*?)\)', lineSplit[1]).group(0)[1:-1].strip().replace('\x00', '')
+
+                    # extract value
+                    value = re.search('\[(.*?)\]', line)
+                    if value != None:
+                        value = value.group(0)[1:-1].strip().replace('\x00', '')
+                    else:
+                        value = 'no value provided'
+
+                    # extract label
+                    label = lineSplit[-1].strip()
+
+                    data[-1][label] = {}
+                    data[-1][label]['tag'] = tag
+                    data[-1][label]['value'] = value
+                    data[-1][label]['label'] = label
+
+        return data
+
+    def formatResponse(self, raw_response):
+        std = raw_response.stdout.decode('ascii')
+        response = {
+            'status':   'success',
+            'data':     '',
+            'command':  raw_response.args
+        }
+
+        status = self.checkResponse(std)
+        if status == 'error':
+            response['status']  = 'error'
+            response['data']    = std
+        else:
+            response['status']  = 'success'
+            response['data']    = self.parseResponse(std)
 
         return response
