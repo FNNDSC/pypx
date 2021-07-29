ab74e0bd3e82ea36c754fab92207c9f147c687b0
Rudolph Pienaar <rudolph.pienaar@childrens.harvard.edu>
2021-07-29 16:38:50 -0400

    Bump to 3.2.6

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def readme():
 
 setup(
         name                =   'pypx',
-        version             =   '3.2.4',
+        version             =   '3.2.6',
         description         =   'PACS/ChRIS core tools and utils',
         long_description    =   readme(),
         url                 =   'http://github.com/fnndsc/pypx',
