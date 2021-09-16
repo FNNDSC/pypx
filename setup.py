963ff616c5538d428afa8ac114e5e67b9989c20b
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2021-09-16 16:55:47 -0400

    Bump to 3.2.52 to force pickup of latest register code

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def readme():
 
 setup(
         name                =   'pypx',
-        version             =   '3.2.50',
+        version             =   '3.2.52',
         description         =   'PACS/ChRIS core tools and utils',
         long_description    =   readme(),
         url                 =   'http://github.com/fnndsc/pypx',
