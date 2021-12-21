42b11a76b8a52b6f34128c27612e0c2d92efdc95
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2021-12-21 12:19:37 -0500

    Bump to 3.4.10

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def readme():
 
 setup(
         name                =   'pypx',
-        version             =   '3.4.8',
+        version             =   '3.4.10',
         description         =   'PACS/ChRIS core tools and utils',
         long_description    =   readme(),
         url                 =   'http://github.com/fnndsc/pypx',
