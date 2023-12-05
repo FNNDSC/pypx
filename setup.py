499fc70a29f401773e9cc06fe9632435fb9e840d
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2023-12-05 17:15:33 -0500

    Bump to 3.11.8

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 setup(
         name                =   'pypx',
-        version             =   '3.11.6',
+        version             =   '3.11.8',
         description         =   'PACS/ChRIS core tools and utils',
         long_description    =   readme(),
         python_requires     =   ">= 3.8",
