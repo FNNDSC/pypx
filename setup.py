fc76273cbad80f7706d0dde571c589cab6649a8f
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2023-12-28 15:19:38 -0500

    Bump to 3.11.18 with new smdb file locking check

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 setup(
         name                =   'pypx',
-        version             =   '3.11.16',
+        version             =   '3.11.18',
         description         =   'PACS/ChRIS core tools and utils',
         long_description    =   readme(),
         python_requires     =   ">= 3.8",
