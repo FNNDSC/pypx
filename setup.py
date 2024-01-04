58b87a4ac03889e017800689328458b95a6e5019
sandipsamal117@gmail.com <sandipsamal117@gmail.com>
2024-01-04 18:46:57 -0500

    version update to 3.11.20

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 setup(
         name                =   'pypx',
-        version             =   '3.11.18',
+        version             =   '3.11.20',
         description         =   'PACS/ChRIS core tools and utils',
         long_description    =   readme(),
         python_requires     =   ">= 3.8",
